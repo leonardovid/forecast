0fb2b9830055060a80534b4d2a933c33ec893ae5
Leonardo Vidal <lenardo.t.vidal@gmail.com>
2017-01-27 03:08:33 -0300

    Body parser añadido

--- server/routes.js
+++ server/routes.js
@@ -1,14 +1,19 @@
-
+var bodyParser = require('body-parser');
 var express = require('express');
 var router = express.Router();
 
+//Body parser para x-www-form-urlencoded
+router.use(bodyParser.urlencoded({ extended: false }))
+
+//Body parser para JSON
+router.use(bodyParser.json())
 
 router.get('/', function(req, res) {
   res.render('search');
 });
 
 router.post('/',function(req, res){
-
+res.send(req.body);
 });
 
 module.exports = router;
