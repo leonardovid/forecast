64255e193acf234e502ab9eeb51e076fe02800ea
Leonardo Vidal <lenardo.t.vidal@gmail.com>
2017-01-27 03:10:44 -0300

    app.js corregido

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 var express = require('express')
 var exphbs  = require('express-handlebars');
-var bodyParser = require('body-parser');
+
 var app = express()
 
 //Archivos estaticos
@@ -15,17 +15,6 @@ app.use(require("./server/routes.js"));
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars')
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
-
-//Body parser para JSON
-app.use(bodyParser.json())
-
-app.use(function (req, res) {
-  res.setHeader('Content-Type', 'text/plain')
-  res.write('you posted:\n')
-  res.end(JSON.stringify(req.body, null, 2))
-})
 
 app.listen(3000, function () {
   console.log('App listening on port 3000!')
